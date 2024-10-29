37ddfd6d54a1eb796cc8d58c9a10f9a3762dc702
takealook97 <takealook97@naver.com>
2024-10-29 12:02:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -0,0 +1,56 @@
+class Solution {
+    static int N, M;
+    static int[] dy = {-1, 0, 1}, dx = {1, 1, 1};
+
+    public int maxMoves(int[][] grid) {
+        N = grid.length;
+        M = grid[0].length;
+        int answer = 1;
+
+        int[][] dp = new int[N][M];
+        for(int i = 0; i < N; i++) {
+            dp[i][0] = 1;
+        }
+
+        int nextY, nextX;
+        for(int nowY = 0; nowY < N; nowY++) {
+            for(int nowX = 0; nowX < M; nowX++) {
+                if(dp[nowY][nowX] <= 0) {
+                    continue;
+                }
+                
+                for(int i = 0; i < 3; i++) {
+                    nextY = nowY + dy[i];
+                    nextX = nowX + dx[i];
+                    if(isInRange(nextY, nextX) && grid[nowY][nowX] < grid[nextY][nextX]) {
+                        dp[nextY][nextX] = Math.max(dp[nextY][nextX], dp[nowY][nowX] + 1);
+                        answer = Math.max(answer, dp[nextY][nextX]);
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return answer - 1;
+    }
+
+    static boolean isInRange(int y, int x) {
+        return 0 <= y && y < N && 0 <= x && x < M;
+    }
+}

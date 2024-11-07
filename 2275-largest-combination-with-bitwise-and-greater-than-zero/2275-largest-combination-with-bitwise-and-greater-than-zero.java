443c74a0b43cf81e6ee50998dfe361365d561783
takealook97 <takealook97@naver.com>
2024-11-07 10:38:48 +0900

    Time: 31 ms (93.65%), Space: 56.3 MB (72.91%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -2,25 +2,19 @@ class Solution {
     static final int LIMIT = 25;
 
     public int largestCombination(int[] candidates) {
-        int N = candidates.length;
         int answer = 0;
-
         int[] count = new int[LIMIT];
 
-        String binary;
-        int len;
         for (int num : candidates) {
-            binary = Integer.toBinaryString(num);
-            len = binary.length();
-            for (int i = len - 1; i >= 0; i--) {
-                if(binary.charAt(i) == '1') {
-                    count[len - 1 - i]++;
+            for (int i = 0; i < LIMIT; i++) {
+                if ((num & (1 << i)) != 0) {
+                    count[i]++;
                 }
             }
         }
 
-        for (int i : count) {
-            answer = Math.max(answer, i);
+        for (int cnt : count) {
+            answer = Math.max(answer, cnt);
         }
 
         return answer;

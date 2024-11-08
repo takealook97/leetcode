c304c4e7a4241e74ad0678aa7fa2cc84ff5991ae
takealook97 <takealook97@naver.com>
2024-11-08 12:53:28 +0900

    Time: 3 ms (78.95%), Space: 58.9 MB (39.46%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -1,43 +1,20 @@
 class Solution {
-    static int len;
-    static int[] count, answer;
-
-    static final int LIMIT = 20;
-
     public int[] getMaximumXor(int[] nums, int maximumBit) {
-        len = nums.length;
-        count = new int[LIMIT];
-        answer = new int[len];
-
-        setCount(nums);
-        find(nums, maximumBit);
-
-        return answer;
-    }
-
-    static void setCount(int[] nums) {
+        int len = nums.length;
+        int[] answer = new int[len];
+        
+        int mask = (1 << maximumBit) - 1;
+        
+        int xor = 0;
         for (int num : nums) {
-            for (int i = 0; i < LIMIT; i++) {
-                if ((num & (1 << i)) != 0) {
-                    count[i]++;
-                }
-            }
+            xor ^= num;
         }
-    }
-
-    static void find(int[] nums, int maximumBit) {
-        for (int i = len - 1; i >= 0; i--) {
-            for (int j = maximumBit - 1; j >= 0; j--) {
-                if (count[j] % 2 == 0) {
-                    answer[len - 1 - i] += Math.pow(2, j);
-                }
-            }
-
-            for (int j = 0; j < LIMIT; j++) {
-                if ((nums[i] & (1 << j)) != 0) {
-                    count[j]--;
-                }
-            }
+        
+        for (int i = 0; i < len; i++) {
+            answer[i] = xor ^ mask;
+            xor ^= nums[len - 1 - i];
         }
+        
+        return answer;
     }
 }

ba2ee6a882d6eb0d9760b67f09c487fee3ff902a
takealook97 <takealook97@naver.com>
2024-11-12 14:52:06 +0900

    Time: 48 ms (43.48%), Space: 81.2 MB (63.31%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
@@ -3,30 +3,33 @@ import java.util.Arrays;
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
         Arrays.sort(items, (a, b) -> a[0] - b[0]);
-        int maxPrice = items[items.length - 1][0];
-        int[] dp = new int[maxPrice + 1];
+        int[] maxBeautyByPrice = new int[items.length];
         
-        int maxBeauty = 0;
-        for (int i = 0; i < items.length; i++) {
-            maxBeauty = Math.max(maxBeauty, items[i][1]);
-            dp[items[i][0]] = maxBeauty;
-        }
-
-        for (int i = 1; i <= maxPrice; i++) {
-            dp[i] = Math.max(dp[i], dp[i - 1]);
+        maxBeautyByPrice[0] = items[0][1];
+        for (int i = 1; i < items.length; i++) {
+            maxBeautyByPrice[i] = Math.max(maxBeautyByPrice[i - 1], items[i][1]);
         }
 
         int[] answer = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            if (queries[i] > maxPrice) {
-                answer[i] = dp[maxPrice];
-            } else if (queries[i] < items[0][0]) {
-                answer[i] = 0;
-            } else {
-                answer[i] = dp[queries[i]];
-            }
+            int query = queries[i];
+            int idx = binarySearch(items, query);
+            answer[i] = idx == -1 ? 0 : maxBeautyByPrice[idx];
         }
 
         return answer;
+    }
+
+    private int binarySearch(int[][] items, int target) {
+        int left = 0, right = items.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (items[mid][0] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
     }
 }

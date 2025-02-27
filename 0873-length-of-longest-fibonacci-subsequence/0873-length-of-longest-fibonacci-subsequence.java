9ae33386c90ddf340e80d29b08234b02cfc3b11c
takealook97 <takealook97@naver.com>
2025-02-27 13:03:29 +0900

    Time: 163 ms (70.41%), Space: 45.3 MB (65.09%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
@@ -1,40 +1,30 @@
 class Solution {
-    static int len, answer;
-    static int[] arr;
-
     public int lenLongestFibSubseq(int[] arr) {
-        len = arr.length;
-        this.arr = arr;
-        answer = 0;
+        int len = arr.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> dp = new HashMap<>();
+        int answer = 0;
 
-        for (int i = 0; i < len - 1; i++) {
+        for (int i = 0; i < len; i++) {
+            map.put(arr[i], i);
+        }
+
+        for (int i = 0; i < len; i++) {
             for (int j = i + 1; j < len; j++) {
-                find (arr[i], arr[j], 0);
+                int x = arr[i], y = arr[j];
+                int count = 2;
+                
+                while (map.containsKey(x + y)) {
+                    int next = x + y;
+                    x = y;
+                    y = next;
+                    count++;
+
+                    answer = Math.max(answer, count);
+                }
             }
         }
 
-        return answer == 0 ? 0 : answer + 2;
-    }
-
-    static void find(int prev, int cur, int count) {
-        answer = Math.max(answer, count);
-        if (lowerBound(prev + cur)) {
-            find(cur, prev + cur, count + 1);
-        }
-    }
-
-    static boolean lowerBound(int target) {
-        int lo = 0, hi = len - 1, mid;
-        
-        while (lo <= hi) {
-            mid = lo + (hi - lo) / 2;
-            if (arr[mid] < target) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-
-        return lo < len && arr[lo] == target;
+        return answer >= 3 ? answer : 0;
     }
 }

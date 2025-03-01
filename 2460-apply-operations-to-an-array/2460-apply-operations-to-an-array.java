62cb1f095473342b60feed88d7e3dc2897e4e853
takealook97 <takealook97@naver.com>
2025-03-01 12:48:32 +0900

    Time: 1 ms (96.37%), Space: 44.5 MB (8.68%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int[] answer = new int[len];
+        int idx = 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != 0) {
+                answer[idx++] = nums[i];
+            }
+        }
+
+        return answer;
+    }
+}

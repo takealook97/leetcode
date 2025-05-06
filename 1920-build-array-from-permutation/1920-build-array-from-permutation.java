f14bbbbd711e7f511c4b2046e08b50506488825c
takealook97 <takealook97@naver.com>
2025-05-06 16:34:00 +0900

    Time: 1 ms (98.72%), Space: 44.9 MB (98.07%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int len = nums.length;
+        int[] answer = new int[len];
+        for (int i = 0; i < len; i++) {
+            answer[i] = nums[nums[i]];
+        }
+        
+        return answer;
+    }
+}

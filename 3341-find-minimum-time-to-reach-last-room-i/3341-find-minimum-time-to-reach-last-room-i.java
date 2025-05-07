b031f00b9189ba9f3a98b31fd6f1348ec3d5931e
takealook97 <takealook97@naver.com>
2025-05-07 15:40:30 +0900

    Time: 6 ms (98.75%), Space: 44.6 MB (87.54%) - LeetHub

---
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.java
@@ -0,0 +1,58 @@
+class Solution {
+    static int[] dy = {-1, 1, 0, 0}, dx = {0, 0, -1, 1};
+
+    static class Point implements Comparable<Point> {
+        int y, x, time;
+
+        public Point(int y, int x, int time) {
+            this.y = y;
+            this.x = x;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return this.time - o.time;
+        }
+    }
+
+    public int minTimeToReach(int[][] moveTime) {
+        int N = moveTime.length, M = moveTime[0].length;
+        int[][] dist = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        dist[0][0] = 0;
+        pq.add(new Point(0, 0, 0));
+
+        while (!pq.isEmpty()) {
+            Point now = pq.poll();
+            if (now.y == N - 1 && now.x == M - 1) return now.time;
+            if (now.time > dist[now.y][now.x]) continue;
+
+            for (int i = 0; i < 4; i++) {
+                int nextY = now.y + dy[i];
+                int nextX = now.x + dx[i];
+                if (0 <= nextY && nextY < N && 0 <= nextX && nextX < M) {
+                    int start = now.time;
+                    if (start >= moveTime[nextY][nextX]) {
+                        start += 1;
+                    } else {
+                        start = moveTime[nextY][nextX] + 1;
+                    }
+
+                    if (start < dist[nextY][nextX]) {
+                        dist[nextY][nextX] = start;
+                        pq.add(new Point(nextY, nextX, start));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}

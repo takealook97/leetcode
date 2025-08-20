34f1e2c42e3832828d05b6d49c1f5160c65cc155
takealook97 <takealook97@naver.com>
2025-08-20 12:45:25 +0900

    Time: 9 ms (12.82%), Space: 57 MB (6.72%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,37 +1,47 @@
 class Solution {
     static int N, M;
+    static int[][] board;
 
     public int countSquares(int[][] matrix) {
         N = matrix.length;
         M = matrix[0].length;
-
+        this.board = matrix;
         int answer = 0;
-        for(int y = 0; y < N; y++) {
-            for(int x = 0; x < M; x++) {
-                for(int gap = 0; gap <= N; gap++) {
-                    if(isInRange(y + gap, x + gap) && isSquare(matrix, y, x, y + gap, x + gap)) {
+
+        for (int fromY = 0; fromY < N; fromY++) {
+            for (int fromX = 0; fromX < M; fromX++) {
+                int gap = 0;
+                while (fromY + gap < N && fromX + gap < M) {
+                    int toY = fromY + gap;
+                    int toX = fromX + gap;
+
+                    if (isSquare(fromY, fromX, toY, toX)) {
                         answer++;
+                        gap++;
+                        continue;
                     }
+
+                    break;
                 }
             }
         }
-        
+
         return answer;
     }
 
-    static boolean isSquare(int[][] matrix, int fromY, int fromX, int toY, int toX) {
-        for(int y = fromY; y <= toY; y++) {
-            for(int x = fromX; x <= toX; x++) {
-                if(matrix[y][x] != 1) {
-                    return false;
-                }
+    static boolean isSquare(int fromY, int fromX, int toY, int toX) {
+        for (int y = fromY; y <= toY; y++) {
+            if (board[y][toX] == 0) {
+                return false;
+            }
+        }
+
+        for (int x = fromX; x <= toX; x++) {
+            if (board[toY][x] == 0) {
+                return false;
             }
         }
 
         return true;
-    }
-
-    static boolean isInRange(int y, int x) {
-        return 0 <= y && y < N && 0 <= x && x < M;
     }
 }

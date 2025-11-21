52da20a72ddda476bf28c6250adba255be809d49
takealook97 <takealook97@naver.com>
2025-11-21 16:34:57 +0900

    Time: 44 ms (93.01%), Space: 47.2 MB (12.46%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,32 +1,68 @@
 class Solution {
+    static Map<Character, Integer> lenMap;
+    static Map<Character, int[]> idxMap;
+    static int answer;
+
+    static final int SIZE = 26;
+
     public int countPalindromicSubsequence(String s) {
         int len = s.length();
-        Set<Character> set = new HashSet<>();
-        Set<Character> temp = new HashSet<>();
+        lenMap = new HashMap<>();
+        idxMap = new HashMap<>();
         for (char c : s.toCharArray()) {
-            set.add(c);
+            lenMap.put(c, lenMap.getOrDefault(c, 0) + 1);
         }
-        int answer = 0;
 
-        for (Character c : set) {
-            int x = -1;
-            int y = 0;
-            for (int i = 0; i < len; i++) {
-                if (c == s.charAt(i)) {
-                    if (x == -1) {
-                        x = i;
-                    }
-                    y = i;
+        for (int i = 0; i < SIZE; i++) {
+            char c = (char) ('a' + i);
+            if (lenMap.containsKey(c)) {
+                idxMap.put(c, new int[lenMap.get(c)]);
+            }
+        }
+
+        int[] idxArr = new int[SIZE];
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            int idx = c - 'a';
+            int[] arr = idxMap.get(c);
+            arr[idxArr[idx]++] = i;
+        }
+
+        answer = 0;
+
+        for (char key : idxMap.keySet()) {
+            int[] val = idxMap.get(key);
+            int from = val[0];
+            int to = val[val.length - 1];
+            if (to - from <= 1) {
+                continue;
+            }
+
+            for (char c : idxMap.keySet()) {
+                int[] arr = idxMap.get(c);
+                // from 보다 크면서, to 보다 작은 인덱스가 arr에 있는지 판별
+                int result = upperBound(arr, from);
+                if (result < arr.length && from < arr[result] && arr[result] < to) {
+                    answer++;
                 }
             }
-
-            for (int i = x + 1; i < y; i++) {
-                temp.add(s.charAt(i));
-            }
-            answer += temp.size();
-            temp.clear();
         }
 
         return answer;
+    }
+
+    static int upperBound(int[] arr, int target) {
+        int lo = 0, hi = arr.length - 1, mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] <= target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return lo;
     }
 }

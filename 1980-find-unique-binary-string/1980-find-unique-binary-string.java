5678a10e5a08376c83f508690a324115dbe76c46
takealook97 <takealook97@naver.com>
2026-03-08 12:42:12 +0900

    Time: 2 ms (40.49%), Space: 43.2 MB (64.7%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,37 +1,37 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         int len = nums.length;
-        int size = (int) Math.pow(2, nums[0].length());
+        Arrays.sort(nums);
         int[] arr = new int[len];
-        boolean[] visited = new boolean[size];
         for (int i = 0; i < len; i++) {
             arr[i] = Integer.parseInt(nums[i], 2);
-        }
-        Arrays.sort(arr);
-        
-        for (int i = 0; i < len; i++) {
-            visited[arr[i]] = true;
-        }
-
-        String answer = null;
-        for (int i = 0; i < size; i++) {
-            if (!visited[i]) {
-                answer = Integer.toBinaryString(i);
-                break;
+            if (i > 0) {
+                if (arr[i] - arr[i - 1] > 1) {
+                    int num = arr[i - 1] + 1;
+                    String bin = Integer.toBinaryString(num);
+                    int gap = nums[i].length() - bin.length();
+                    StringBuilder sb = new StringBuilder();
+                    while (gap-- > 0) {
+                        sb.append(0);
+                    }
+                    sb.append(bin);
+                    return sb.toString();
+                }
             }
         }
-
-        int gap = nums[0].length() - answer.length();
-        if (gap == 0) {
-            return answer;
+        boolean isZero = false;
+        if (arr[0] != 0) {
+            isZero = true;
         }
-
         StringBuilder sb = new StringBuilder();
-        while (gap-- > 0) {
-            sb.append(0);
+        int length = nums[0].length();
+        while(length-- > 0) {
+            if (isZero) {
+                sb.append(0);
+            } else {
+                sb.append(1);
+            }
         }
-        sb.append(answer);
-
         return sb.toString();
     }
 }
